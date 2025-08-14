70e10bd3cce242006dd42cd651efc6599ea36d67
Mark McFadden <m2web@yahoo.com>
2025-08-14 17:20:08 -0400

    :bug: fix: Correct instantiation of logprobs in DummyResponse class

--- tests/test_next_token_predictor.py
+++ tests/test_next_token_predictor.py
@@ -13,7 +13,7 @@ class DummyResponse:
             }]
         logprobs = LogProbs()
         def __init__(self):
-            self.logprobs = [self.Choices()]
+            self.logprobs = [Choices()]
     choices = [Choices()]
 
     def __init__(self):
